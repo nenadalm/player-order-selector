04a8082e26d80762a97e094ae0f7f3a604c279f9
Miloslav Nenadal <nenadalm@gmail.com>
2024-02-22 11:54:45 +0100

    Initial commit

---
+++ src/app/subs.cljs
@@ -0,0 +1,25 @@
+(ns app.subs
+  (:require
+   [re-frame.core :as re-frame]))
+
+(re-frame/reg-sub
+ ::touches
+ (fn [db _]
+   (vals (get-in db [:game :touches]))))
+
+(re-frame/reg-sub
+ ::decide-at
+ (fn [db _]
+   (get-in db [:game :decide-at])))
+
+(re-frame/reg-sub
+ ::decided
+ (fn [db _]
+   (= :decided (get-in db [:game :state]))))
+
+(re-frame/reg-sub
+ ::show-help
+ (fn [db _]
+   (let [game (:game db)]
+     (and (= :deciding (:state game))
+          (not (:decide-at game))))))
---
+++ src/build/create_manifest.clj
@@ -0,0 +1,24 @@
+(ns build.create-manifest
+  (:require
+   [jsonista.core :as j]
+   [build.util :as u]))
+
+(defn render [module-id->output-name]
+  (j/write-value-as-string
+   {:name "Player order selector"
+    :description "Random player order selector."
+    :icons [{:src (u/asset "img/icon.svg" module-id->output-name)
+             :sizes "any"
+             :type "image/svg+xml"}
+            {:src (u/asset "img/icon.svg" module-id->output-name)
+             :sizes "any"
+             :type "image/svg+xml"
+             :purpose "maskable"}
+            {:src (u/asset "img/icon_512.png" module-id->output-name)
+             :sizes "512x512"
+             :type "image/png"
+             :purpose "maskable"}]
+    :background_color "#121212"
+    :theme_color "#121212"
+    :display "fullscreen"
+    :start_url "index.html"}))
