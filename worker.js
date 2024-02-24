b25e4c38ad00c93ddac3f8dcca094c811e264681
nenadalm <nenadalm@gmail.com>
2024-02-24 11:23:32 +0000

    Deploying to gh-pages from @ nenadalm/player-order-selector@c6a49f8b3724db754a87ba6d86557c6988171c1b 🚀

---
+++ worker.js
@@ -0,0 +1,37 @@
+const relatedAppVersion = "c6a49f8b3724db754a87ba6d86557c6988171c1b";
+const urlsToCache = ["index.html","manifest.json","img/icon.svg","js/app.8CE1C1A56B11B44EB8C186D06333446B.js","css/styles.45e18180c0bc8737a4bef08bdedb1211cc2eb2df.css"];
+
+const cacheKeyPrefix = 'nenadalm.backgammon.';
+const cacheKey = `${cacheKeyPrefix}resources.${relatedAppVersion}`;
+
+function ensureHtmlVersionMatches(cache) {
+    return cache.match(new Request('index.html'))
+        .then(response => response.text())
+        .then(html => html.match(/<meta name="app-version" content="(.*?)">/)[1])
+        .then(version => {
+            if (version !== relatedAppVersion) {
+                return Promise.reject(`Incorrect index.html version ${version} doesn't match worker.js version ${relatedAppVersion}`);
+            }
+        })
+}
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(cacheKey).then(
+        cache => cache.addAll(urlsToCache)
+            .then(() => ensureHtmlVersionMatches(cache))));
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+            .then(keys => keys.filter(key => key.startsWith(cacheKeyPrefix)))
+            .then(keys => keys.filter(key => key !== cacheKey))
+            .then(oldKeys => Promise.all(oldKeys.map(key => caches.delete(key))))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.open(cacheKey)
+                      .then(cache => cache.match(event.request))
+                      .then(response => response ?? fetch(event.request)));
+});
